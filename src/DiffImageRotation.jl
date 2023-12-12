2eafad03e97a45a2d131cabaee934824b51e53ae
roflmaostc <fxw+git@mailbox.org>
2023-12-12 16:27:20 +0100

    Add Docstring

--- src/DiffImageRotation.jl
+++ src/DiffImageRotation.jl
@@ -10,11 +10,58 @@ imrotate(arr::AbstractArray{T, 2}, θ) where T =
 """
     imrotate(arr::AbstractArray, θ)
 
-Rotates a matrix around the center pixel `size(arr) ÷ 2 .+ 1` which is the real center.
+Rotates a matrix around the center pixel `size(arr) ÷ 2 .+ 1` which means there is a real center pixel 
 The angle `θ` is interpreted in radians.
 
 The adjoint is defined with ChainRulesCore. This method also runs with CUDA.
 If `arr` is a `AbstractArray{T, 3}`, the third dimension is interpreted as batch dimension.
+
+
+# Examples
+```julia-repl
+julia> arr = zeros((6, 6)); arr[3:4, 4] .= 1;
+
+julia> arr
+6×6 Matrix{Float64}:
+ 0.0  0.0  0.0  0.0  0.0  0.0
+ 0.0  0.0  0.0  0.0  0.0  0.0
+ 0.0  0.0  0.0  1.0  0.0  0.0
+ 0.0  0.0  0.0  1.0  0.0  0.0
+ 0.0  0.0  0.0  0.0  0.0  0.0
+ 0.0  0.0  0.0  0.0  0.0  0.0
+
+julia> imrotate(arr, deg2rad(45))
+6×6 view(::Array{Float64, 3}, :, :, 1) with eltype Float64:
+ 0.0  0.0  0.0        0.0        0.0       0.0
+ 0.0  0.0  0.0        0.0        0.0       0.0
+ 0.0  0.0  0.0        0.292893   0.585786  0.0
+ 0.0  0.0  0.0857864  1.0        0.292893  0.0
+ 0.0  0.0  0.0        0.0857864  0.0       0.0
+ 0.0  0.0  0.0        0.0        0.0       0.0
+
+julia> imrotate(arr, deg2rad(90))
+6×6 view(::Array{Float64, 3}, :, :, 1) with eltype Float64:
+ 0.0  0.0  0.0  0.0  0.0          0.0
+ 0.0  0.0  0.0  0.0  0.0          0.0
+ 0.0  0.0  0.0  0.0  1.11022e-16  0.0
+ 0.0  0.0  0.0  1.0  1.0          0.0
+ 0.0  0.0  0.0  0.0  0.0          0.0
+ 0.0  0.0  0.0  0.0  0.0          0.0
+
+julia> using CUDA
+
+julia> Array(imrotate(CuArray(arr), deg2rad(90))) ≈ imrotate(arr, deg2rad(90))
+true
+
+julia> using Zygote
+
+julia> f(x) = sum(abs2.(imrotate(x, π/4)))
+f (generic function with 1 method)
+
+julia> Zygote.gradient(f, arr)
+([0.0 0.0 … 0.0 0.0; 0.0 0.0 … 5.387705551013979e-17 0.0; … ; 0.0 0.0 … 0.08578643762690495 0.0; 0.0 0.0 … 0.0 0.0],)
+
+```
 """
 function imrotate(arr::AbstractArray{T, 3}, θ) where T
 	@assert size(arr, 1) == size(arr, 2) "only quadratic arrays in dimension 1 and 2"
