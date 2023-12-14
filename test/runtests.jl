8b881b2bca29bcad3e24dc70ddd007e1876f7526
roflmaostc <fxw+git@mailbox.org>
2023-12-14 10:32:01 +0100

    Make gradient testing more robust

--- test/runtests.jl
+++ test/runtests.jl
@@ -55,20 +55,19 @@ using Zygote, FiniteDifferences, ImageTransformations
         f(x) = sum(abs2.(DiffImageRotation.imrotate(x, deg2rad(137 ))))
        
         img2 = randn((32, 32));
-        grad = FiniteDifferences.grad(central_fdm(5, 1), f, img2)[1]
+        grad = FiniteDifferences.grad(central_fdm(7, 1), f, img2)[1]
         grad2 = Zygote.gradient(f, img2)[1];
-        @test grad ≈ grad2
+        @test all(.≈(1 .+ grad, 1 .+ grad2, rtol=1f-8))
         
         img2 = randn((21, 21));
-        grad = FiniteDifferences.grad(central_fdm(5, 1), f, img2)[1]
+        grad = FiniteDifferences.grad(central_fdm(7, 1), f, img2)[1]
         grad2 = Zygote.gradient(f, img2)[1];
-        @test grad ≈ grad2
-
+        @test all(.≈(1 .+ grad, 1 .+ grad2, rtol=1f-8))
 
         img2 = randn((20, 14, 3));
-        grad = FiniteDifferences.grad(central_fdm(5, 1), f, img2)[1]
+        grad = FiniteDifferences.grad(central_fdm(7, 1), f, img2)[1]
         grad2 = Zygote.gradient(f, img2)[1];
-        @test grad ≈ grad2
+        @test all(.≈(1 .+ grad, 1 .+ grad2, rtol=1f-8))
     end
 
 
